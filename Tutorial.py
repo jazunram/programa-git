b78ca9fa27d70d06db6ceb1bf10ca6b1ff8654aa
jazunram <zuniga.1082@gmail.com>
2025-07-10 11:08:13 -0600

    Version 1

---
+++ Tutorial.py
@@ -0,0 +1,7 @@
+#Bloque Identificativo
+#Programa: Calculadora de Área y Perímetro de un Rectángulo
+#Descripción: Este programa calcula el área y el perímetro de un rectángulo dados su ancho y su alto
+#Autor:[Juan Zúñiga]
+#Fecha: 10/jul/2025
+#Bloque Declarativo e informativo
+conda
